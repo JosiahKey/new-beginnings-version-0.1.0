22209fa8915c4599b68a3b59d3f871bfe52786e2
JosiahKey <josiah.key@gmail.com>
2025-12-07 19:22:26 -0500

    new level5

--- Scripts/interactable.gd
+++ Scripts/interactable.gd
@@ -1,27 +1,22 @@
 extends Area2D
 
-var active: bool = false
-
-func _on_next_level_entered(body):
-	if body.is_in_group("Player"):
-		print("_on_nextlevel_entered()")
-		#SignalBus.load_area_entered.emit()
+var in_range: bool = false
+var collected: bool = false
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerCursor"):
-		print("_on_test_entered()")
-		active = true
+		if(!collected):
+			in_range = true
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("PlayerCursor"):
 		print("_on_test_axited()")
-		active = false
+		in_range = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Interact"):
-		if active:
+		if in_range and !collected:
 			SignalBus.item_generated.emit()
-			print("success")
-			self.queue_free()
-		else:
-			print("fail")
+			collected = true
+			$TextureRect.visible = false
+			$AudioStreamPlayer2D.playing = true
