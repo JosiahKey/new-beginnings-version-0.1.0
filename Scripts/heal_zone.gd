ec3c10e1ac758dd9414cca655ec750e4505f1fc7
JosiahKey <josiah.key@gmail.com>
2026-01-01 13:13:39 -0500

    Update heal_zone.gd
    -fixed hp progress bar doesn't move when using heal zone

--- Scripts/heal_zone.gd
+++ Scripts/heal_zone.gd
@@ -3,4 +3,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		PlayerData.stat_data["Current_hp"] = PlayerData.stat_data["Total_hp"]
+		SignalBus.update_stat_panel.emit()
 		$SFX.playing = true
