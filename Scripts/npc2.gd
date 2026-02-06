a73d194b456435bfd6410b8a36ab60b45a9646dc
JosiahKey <josiah.key@gmail.com>
2026-02-05 19:41:44 -0500

    ugh

--- Scripts/npc2.gd
+++ Scripts/npc2.gd
@@ -8,11 +8,11 @@ var collected: bool = false
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("PlayerCursor"):
 		in_range = true
-		if PlayerData.equipment_data["Mainhand"] != 0:
-			if GameData.item_data[PlayerData.equipment_data["Mainhand"]]["item_rarity"] == "rare" or "epic" or "legendary":
-				$Dialouge/MarginContainer/Label.text = "Nice Weapon!"
-			else:
-				$Dialouge/MarginContainer/Label.text = "This place is too dangerous. Go ask the Smith for a better weapon."
+		#if PlayerData.equipment_data["Mainhand"] != 0:
+			#if GameData.item_data[PlayerData.equipment_data["Mainhand"]]["item_rarity"] == "rare" or "epic" or "legendary":
+				#$Dialouge/MarginContainer/Label.text = "Nice Weapon!"
+			#else:
+				#$Dialouge/MarginContainer/Label.text = "This place is too dangerous. Go ask the Smith for a better weapon."
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("PlayerCursor"):
